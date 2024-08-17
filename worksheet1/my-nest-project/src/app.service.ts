4cfa51a97b6a7894e19faaf9ef7c5eed364b6076
bbag199 <148782320+bbag199@users.noreply.github.com>
2024-08-18 01:55:54 +1200

    Update app.service.ts

--- worksheet1/my-nest-project/src/app.service.ts
+++ worksheet1/my-nest-project/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'NEST PROJECT WORKSHEET 1 RAAAHHHHH';
   }
 }
